0ed322b19abe10a71acca838d75660a06335ccb5
mathbr10 <110001078+mathbr10@users.noreply.github.com>
2023-01-19 07:35:51 -0300

    Enable ssh

--- bentoctl_aws_ec2/templates/terraform_default.tf
+++ bentoctl_aws_ec2/templates/terraform_default.tf
@@ -107,14 +107,14 @@ resource "aws_security_group" "allow_bentoml" {
   }
 
   # Uncomment if you want to ennable ssh access into the instance. Not for prod.
-  # ingress {
-  #   description      = "ssh access (incase, for debugging)"
-  #   from_port        = 22
-  #   to_port          = 22
-  #   protocol         = "tcp"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  #   ipv6_cidr_blocks = ["::/0"]
-  # }
+  ingress {
+    description      = "ssh access (incase, for debugging)"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
